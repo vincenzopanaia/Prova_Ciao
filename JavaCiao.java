8341ab712518309d54c95cacef2c1dbbb69d9780
vincenzopanaia <34100547+vincenzopanaia@users.noreply.github.com>
2017-11-29 16:25:20 +0100

    CLASSE

--- JavaCiao.java
+++ JavaCiao.java
@@ -1,5 +1,7 @@
+ public class Cocco() { //LA CLASSE VA PRIMA DEL METODO, DIOCRISTO!
+
 public static void main(String[] args) { // questo è il commento di Cocco
-  public class Cocco() {
+ 
     //proviamo ad implementare un metodo
     Vincenzo v = new Vincenzo []; //Array di Vincenzo
     System.out.println ("Sei un gattoNE!")
