3a41e8ac27f11df04e657f4dde9200a26244ccaf
vadakk <34094967+vadakk@users.noreply.github.com>
2017-11-29 16:22:30 +0100

    Update JavaCiao.java

--- JavaCiao.java
+++ JavaCiao.java
@@ -3,6 +3,7 @@ public static void main(String[] args) { // questo è il commento di Cocco
     //proviamo ad implementare un metodo
     Vincenzo v = new Vincenzo []; //Array di Vincenzo
     System.out.println ("Sei un gattoNE!")
+      //sono un virus!!!
   }
 System.out.println ("Ciao GitHub");
 }
