598ad616df23eea9097d131dd4e74142c7e486c0
cocco88 <34098023+cocco88@users.noreply.github.com>
2017-11-29 16:25:21 +0100

    Update JavaCiao.java

--- JavaCiao.java
+++ JavaCiao.java
@@ -1,9 +1,9 @@
 public static void main(String[] args) { // questo è il commento di Cocco
   public class Cocco() {
     //proviamo ad implementare un metodo
-    Vincenzo v = new Vincenzo []; //Array di Vincenzo
+    Vincenzo v = new Vincenzo [sono un coglione]; //Array di Vincenzo
+    
     System.out.println ("Sei un gattoNE!")
-      //sono un virus!!!
   }
 System.out.println ("Ciao GitHub");
 }
