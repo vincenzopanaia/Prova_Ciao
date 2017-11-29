424e69af768a9e2852135990f87e42a4d66f330a
cocco88 <34098023+cocco88@users.noreply.github.com>
2017-11-29 16:12:46 +0100

    Aggiorniamo con metodo
    Solo commento metodo!

--- JavaCiao.java
+++ JavaCiao.java
@@ -1,3 +1,7 @@
 public static void main(String[] args) { // questo è il commento di Cocco
+  public class Cocco() {
+    //proviamo ad implementare un metodo
+    
+  }
 System.out.println ("Ciao GitHub");
 }
